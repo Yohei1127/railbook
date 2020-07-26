7c8c9328c3b846df318d6efbe5a380b55f17b758
Yohei1127 <yohei.fujii1127@gmail.com>
2020-07-26 16:58:34 +0900

    feat: create books scaffold application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200726070533) do
+ActiveRecord::Schema.define(version: 20200726073913) do
 
   create_table "books", force: :cascade do |t|
     t.string "isbn"
